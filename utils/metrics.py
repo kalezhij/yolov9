287ee30c1ac349268b376c7eb691152d3c051d76
kalezhij <kalezhi@gmail.com>
2024-08-04 22:00:32 +0200

    metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -363,7 +363,7 @@ def wh_iou(wh1, wh2, eps=1e-7):
 @threaded
 def plot_pr_curve(px, py, ap, save_dir=Path('pr_curve.png'), names=()):
     # Precision-recall curve
-    fig, ax = plt.subplots(1, 1, figsize=(12, 6), tight_layout=True)
+    fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
     py = np.stack(py, axis=1)
 
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
@@ -377,7 +377,7 @@ def plot_pr_curve(px, py, ap, save_dir=Path('pr_curve.png'), names=()):
     ax.set_ylabel('Precision',fontsize=20)
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
-    ax.legend(bbox_to_anchor=(1.04, 1),fontsize="16", loc="upper left")
+    ax.legend(bbox_to_anchor=(1.04, 1),fontsize="15", loc="upper left")
     ax.set_title('Precision-Recall Curve',fontsize=20)
     plt.setp(ax.get_xticklabels(), fontsize=20)
     plt.setp(ax.get_yticklabels(), fontsize=20)
